d2d40424f56e1f901db12705a5a94d5731872338
ngozz <hieungoanngoan@gmail.com>
2023-07-03 10:50:30 +0700

    update

--- DatPhong/js/DatPhong.js
+++ DatPhong/js/DatPhong.js
@@ -23,3 +23,61 @@ window.onclick = function (event) {
         modal.style.display = "none";
     }
 }
+
+  
+document.getElementById("myBtn").addEventListener("click", function(event) {
+    event.preventDefault();
+  
+    // Collect data from form
+    let hoten = document.getElementById("hoten").value;
+    let email = document.getElementById("email").value;
+    let sdt = document.getElementById("sdt").value;
+    let diachi = document.getElementById("diachi").value;
+    let checkin = document.getElementById("checkin").value;
+    let checkout = document.getElementById("checkout").value;
+    let soluongkhach = document.getElementById("soluongkhach").value;
+    let soluongtreem = document.getElementById("soluongtreem").value;
+    let kieuphong = document.getElementById("kieuphong").value;
+    let ghichu = document.getElementById("ghichu").value;
+  
+    // Collect values of checked options
+    let dichvu = [];
+    let checkboxes = document.querySelectorAll('input[type=checkbox]:checked');
+    for (let checkbox of checkboxes) {
+      dichvu.push(checkbox.value);
+    }
+  
+    // Create JSON object
+    let data = {
+      TenKhachHang: hoten,
+      SDT: sdt,
+      Email: email,
+      DiaChi: diachi,
+      SoNguoiLon: soluongkhach,
+      SoTreEm: soluongtreem,
+      NgayDenNhan: checkin,
+      NgayTra: checkout,
+      MaLoaiPhong: kieuphong,
+      MaDichVu: dichvu,
+      GhiChu: ghichu
+    };
+  
+    // Print data to console
+    console.log(data);
+  });
+  
+
+    // // Create JSON object
+    // let data = {
+    //   TenKhachHang: hoten,
+    //   SDT: sdt,
+    //   Email: email,
+    //   DiaChi: diachi,
+    //   SoNguoiLon: soluongkhach,
+    //   SoTreEm: soluongtreem,
+    //   NgayDenNhan: checkin,
+    //   NgayTra: checkout,
+    //   MaLoaiPhong: kieuphong,
+    //   //MaDichVu: dichvu,
+    //   GhiChu: ghichu
+    // };
