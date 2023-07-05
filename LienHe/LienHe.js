344b5cb4012c2bc3b946a1cf8726d994703540a8
ngozz <hieungoanngoan@gmail.com>
2023-07-05 21:33:55 +0700

    update

--- LienHe/LienHe.js
+++ LienHe/LienHe.js
@@ -4,6 +4,12 @@ emailjs.init('DbTsP_2pzP-09GtkP');
 // Get the form element
 var form = document.getElementById('myForm');
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  
 // Add an event listener to handle form submission
 form.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -38,6 +44,26 @@ form.addEventListener('submit', function(event) {
                     }, function(error) {
                         swal("Gửi thông tin thất bại, xin vui lòng thử lại");
                     });
+                
+                    let malienhe = getRandomInt(10000, 90000);
+                    let ten = document.getElementById("ten").value;
+                    let email = document.getElementById("email").value;
+                    let sdt = document.getElementById("sdt").value;
+                    let chude = document.getElementById("chude").value;
+                    let noidung = document.getElementById("noidung").value;
+                
+                    // Create JSON object
+                    let data = {
+                        MaLienHe: malienhe,
+                        Ten: ten,
+                        Email: email,
+                        SDT: sdt,
+                        ChuDe: chude,
+                        NoiDung: noidung
+                    };
+                
+                    // Output JSON object
+                    console.log(data);
             } else {
                 swal("Đã huỷ thành công");
             }
@@ -47,3 +73,4 @@ form.addEventListener('submit', function(event) {
         swal("Vui lòng điền đầy đủ thông tin");
     }
 });
+
